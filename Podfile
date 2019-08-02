2340730e3c5a4572cf0a77d990c766b7cd71f463
Vinicius Ricci <vinicius.ricci4@gmail.com>
2019-08-02 00:20:10 -0300

    testing project by cocoa touch framework

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RWPickFlavor' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for RWPickFlavor
+
+  target 'RWPickFlavorTests' do
+	 pod 'Alamofire', '~> 4.7'
+    	pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Vinicius Ricci on 01/08/19.
+//  Copyright © 2019 Vinicius Ricci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
